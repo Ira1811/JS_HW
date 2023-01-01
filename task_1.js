87657dbe9745e0fb759561f42c0467ad75ff1d67
Ira1811 <120781211+Ira1811@users.noreply.github.com>
2023-01-01 15:44:11 +0100

    Add files via upload

---
+++ task_1.js
@@ -0,0 +1,9 @@
+const name = "Protective field generator";
+let price = 1000;
+
+
+console.log( `selected ${name} , price per one ${price} credits`);
+
+let price = 2000;
+
+console.log( `selected ${name} , price per one ${price} credits`);
